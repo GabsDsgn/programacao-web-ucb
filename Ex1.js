8c09fe08a7fd26ee9b21535b1ab34162bf881316
Gabs <102196313+GabsDsgn@users.noreply.github.com>
2024-03-13 16:43:29 -0300

    Comit feito na aula 2

--- Ex1.js
+++ Ex1.js
@@ -4,3 +4,11 @@ Num2 = 6;
 Soma = Num1 + Num2;
 
 console.log(`A soma de ${Num1} e ${Num2} é ${Soma}`);
+
+function Calc(a, b) {
+
+    console.log(`O resultado da subtração entre ${a} e ${b} é ${a + b}`)
+
+}
+
+Calc(4, 6);
