fd72da1b5b0260049f614b68c6102a0bd12b88b7
Gabs <102196313+GabsDsgn@users.noreply.github.com>
2024-03-20 10:29:41 -0300

    commit 3

--- Atividade2/Ex2.js
+++ Atividade2/Ex2.js
@@ -1,19 +1,13 @@
-function verificarNumeroPrimo(numero) {
-    if (numero <= 1) {
+function verificarNumeroPrimo(n) {
+
+    let isPrimo = true;
+
+    if(n<=1) {
         return false;
     }
-    if (numero <= 3) {
-        return true;
-    }
-    if (numero % 2 === 0 || numero % 3 === 0) {
-        return false;
-    }
-    for (let i = 5; i * i <= numero; i += 6) {
-        if (numero % i === 0 || numero % (i + 2) === 0) {
-            return false;
-        }
-    }
+
     return true;
+
 }
 
 console.log(verificarNumeroPrimo(0)); // false
